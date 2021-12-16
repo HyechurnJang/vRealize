5b81f1f5f62e7f00670a1988b9166f56fccc44db
jzide <jzide@JZ5550>
2021-12-16 10:40:01 +0900

    update

---
+++ devs/abx/custom_resource/resources/vpz/resource_update.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 1983. 08. 09.
+@author: Hye-Churn Jang, CMBU Specialist in Korea, VMware [jangh@vmware.com]
+'''
+
+import sys
+import importlib
+import manifest
+sys.path.insert(0, '../../common')
+_module = importlib.import_module(manifest.sdk)
+for exportObject in _module.exportObjects: __builtins__[exportObject] = _module.__getattribute__(exportObject)
+
+# __ABX_IMPLEMENTATIONS_START__
+#===============================================================================
+# ABX Code Implementations                                                     #
+#===============================================================================
+# Import Libraries Here
+
+# Implement Handler Here
+def handler(context, inputs):
+    # set common values
+    vra = VraManager(context, inputs)
+    
+    # retrieve resource
+    resource = vra.get('' + inputs['id'])
+    
+    # update resource
+    if 'var1' in inputs: resource['var1'] = inputs['var1']
+    if 'var2' in inputs: resource['var2'] = inputs['var2']
+    vra.put('' + inputs['id'], resource)
+    
+    # publish resource
+    outputs = inputs
+    outputs.pop('VraManager')
+    return outputs
+# __ABX_IMPLEMENTATIONS_END__
