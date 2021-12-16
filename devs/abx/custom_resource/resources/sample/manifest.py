d5ef88d30855a4a8dfa120938c7447f6a2d93809
jzide <jzide@JZ5550>
2021-12-16 15:17:53 +0900

    init

---
+++ devs/abx/custom_resource/resources/sample/manifest.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 1983. 08. 09.
+@author: Hye-Churn Jang, CMBU Specialist in Korea, VMware [jangh@vmware.com]
+'''
+
+name = '' # custom resource name
+
+sdk = 'no' # imported SDK at common directory
+
+inputs = {
+    'create': {
+        #=======================================================================
+        # name_here: default or constant
+        # example)
+        # 'VraManager': 'constant'
+        #=======================================================================
+    },
+    'read': {
+    },
+    'update': {
+    },
+    'delete': {
+    }
+}
+
+properties = {
+    #===========================================================================
+    # 'string_required_property_name': {
+    #     'type': 'string',
+    #     'title': 'title_here',
+    #     'description': 'description_here',
+    # },
+    # 'string_optional_property_name': {
+    #     'type': 'string',
+    #     'default': '',
+    # },
+    # 'string_selectable_property_name': {
+    #     'type': 'string',
+    #     'enum': ['a', 'b'],
+    # },
+    # 'string_recreate_on_update_property_name': {
+    #     'type': 'string',
+    #     'recreateOnUpdate': True
+    # },
+    # 'string_encrypted_property_name': {
+    #     'type': 'string',
+    #     'encrypted': True
+    # },
+    # 'string_array_property_name': {
+    #     'type': 'array',
+    #     'default': [],
+    #     'items': {
+    #         'type': 'string'
+    #     },
+    # },
+    # 'object_property_name': {
+    #     'type': 'object',
+    #     'default': {},
+    # },
+    #===========================================================================
+}
