7ddc8dee70c12e079d37b9d614305e954a4325ab
jzide <jzide@JZ5550>
2022-05-11 10:34:10 +0900

    update

--- devs/abx/custom_resource/resources/project/resource_delete.py
+++ devs/abx/custom_resource/resources/project/resource_delete.py
@@ -16,6 +16,7 @@ for exportObject in _module.exportObjects: __builtins__[exportObject] = _module.
 # ABX Code Implementations                                                     #
 #===============================================================================
 # Import Libraries Here
+import time
 
 # Implement Handler Here
 def handler(context, inputs):
@@ -23,6 +24,23 @@ def handler(context, inputs):
     vra = VraManager(context, inputs)
     
     # delete resource
+    deployments = vra.get('/deployment/api/deployments?projects={}'.format(inputs['id']))['content']
+    deploymentIds = []
+    for deployment in deployments:
+        deploymentIds.append(deployment['id'])
+        vra.delete('/deployment/api/deployments/{}'.format(deployment['id']))
+    if deploymentIds: time.sleep(2)
+    while deploymentIds:
+        for deploymentId in deploymentIds:
+            try: deployment = vra.get('/deployment/api/deployments/' + deploymentId)
+            except:
+                deploymentIds.remove(deploymentId)
+                break
+            if deployment['status'] == 'DELETE_INPROGRESS': continue
+            else:
+                raise Exception('could not delete deployment : {}'.format(deploymentId))
+        else: time.sleep(5)
+    
     resource = vra.get('/iaas/api/projects/' + inputs['id'])
     resource['zoneAssignmentConfigurations'] = []
     vra.patch('/iaas/api/projects/' + inputs['id'], resource)
